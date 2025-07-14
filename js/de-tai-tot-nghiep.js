32ddc71f8c45dbab838a7e4cc5fdf778d8fa77d8
lengocminh <minhln@dhhp.edu.vn>
2025-07-14 13:27:55 +0700

    Them module Quan ly De tai Tot nghiep

---
+++ js/de-tai-tot-nghiep.js
@@ -0,0 +1,8 @@
+// File: js/de-tai-tot-nghiep.js
+// This file will contain all the specific JavaScript logic
+// for the "Graduation Thesis Management" module.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Trang Quản lý Đề tài Tốt nghiệp đã được tải.');
+    // Logic cho chức năng này sẽ được thêm vào đây trong tương lai.
+});
